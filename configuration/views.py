3c857525ea15f823d5f82a84db415b3abc5f71bd
Denis Pavlov <pavlovdo@gmail.com>
2015-05-20 15:13:57 +0300

    initial project version

---
+++ configuration/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render_to_response
+
+
+def index(request):
+    args = {}
+    return render_to_response('configuration.html', args)
