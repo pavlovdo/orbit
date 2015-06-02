0e13b834030317223b810701f327593b11cd0d22
Denis Pavlov <pavlovdo@gmail.com>
2015-06-02 18:52:56 +0300

    initial project version

--- orbit/settings/base.py
+++ orbit/settings/base.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'configuration',
+    'reports',
 )
 
 MIDDLEWARE_CLASSES = (
--- orbit/urls.py
+++ orbit/urls.py
@@ -19,5 +19,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^configuration/', include('configuration.urls')),
+    url(r'^reports/', include('reports.urls')),
     url(r'^$', 'orbit.views.home', name='home'),
 ]
